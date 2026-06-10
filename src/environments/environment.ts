566bd4905eeedf8c2f1e4fd947d9ad9b808923e0
Abel Lopez <abel.lopez97@outlook.com>
2026-06-10 00:38:14 -0400

    actualizando variables de entorno para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 // Configuración de entorno. apiUrl apunta al backend Spring Boot + GraphQL.
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://rrhh-prod.eba-p8y8badt.us-east-2.elasticbeanstalk.com',
   graphqlPath: '/graphql',
   loginPath: '/api/v1/auth/login',
   refreshPath: '/api/v1/auth/refresh',
-  fastapiGql: 'http://localhost:8001/graphql',
+  fastapiGql: 'https://hr-fastapi.duckdns.org',
 };
